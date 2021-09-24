5e64b803e598dba66705d43ae6e32c0606b05698
musef <musef2904@gmail.com>
2021-09-24 17:00:51 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'symf34-prod-v100';
+}
